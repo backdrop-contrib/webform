c77fb33c4faa06b12397db8ff379bab5c698ca6a
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2010-02-11 22:05:27 +0000

    #520524 by djalloway and quicksketch: Redirect URL and confirmation message should be two different fields.

--- templates/webform-confirmation.tpl.php
+++ templates/webform-confirmation.tpl.php
@@ -17,7 +17,13 @@
  */
 ?>
 
-<div class="webform-confirmation"><?php print $confirmation_message ?></div>
+<div class="webform-confirmation">
+  <?php if ($confirmation_message): ?>
+    <?php print $confirmation_message ?>
+  <?php else: ?>
+    <p><?php print t('Thank you, your submission has been received.'); ?></p>
+  <?php endif; ?>
+</div>
 
 <div class="links">
   <a href="<?php print url('node/'. $node->nid) ?>"><?php print t('Go back to the form') ?></a>
