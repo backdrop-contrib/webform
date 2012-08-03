14a0103f9face9fd7e8f939365d53b76fed9c3f6
Saul Willers <saul@saulwillers.com>
2012-08-03 13:24:01 -0400

    Issue #1512902 by rocketeerbkw and tim.plunkett: Document hook_webform_results_access().

--- webform.api.php
+++ webform.api.php
@@ -468,6 +468,31 @@ function hook_webform_submission_access($node, $submission, $op = 'view', $accou
 }
 
 /**
+ * Determine if a user has access to see the results of a webform.
+ *
+ * Note in addition to the view access to the results granted here, the $account
+ * must also have view access to the Webform node in order to see results.
+ *
+ * @see webform_results_access().
+ *
+ * @param $node
+ *   The Webform node to check access on.
+ * @param $account
+ *   The user account to check access on.
+ * @return
+ *   TRUE or FALSE if the user can access the webform results.
+ */
+function hook_webform_results_access($node, $account) {
+  // Let editors view results of unpublished webforms.
+  if ($node->status == 0 && in_array('editor', $account->roles)) {
+    return TRUE;
+  }
+  else {
+    return FALSE;
+  }
+}
+
+/**
  * Return an array of files associated with the component.
  *
  * The output of this function will be used to attach files to e-mail messages.
