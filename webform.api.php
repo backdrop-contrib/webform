845b5eae3d46e77792d3d75157e2ecf93b98a500
Dan Chadwick <dan899@gmail.com>
2014-07-12 07:27:21 -0400

    Issue #2302029 by DanChadwick: Add nid and uid context for hook_webform_draft_alter.

--- webform.api.php
+++ webform.api.php
@@ -241,8 +241,10 @@ function hook_webform_submission_actions($node, $submission) {
  * @param integer $sid
  *    The id of the most recent submission to be presented for editing. Change
  *    to a different draft's sid or set to NULL for a new draft.
+ * @param array $context
+ *    Array of context with indices 'nid' and 'uid'.
  */
-function hook_webform_draft_alter(&$sid) {
+function hook_webform_draft_alter(&$sid, $context) {
   if ($_GET['newdraft']) {
     $sid = NULL;
   }
